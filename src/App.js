1c65ceabf97cfdb3e2e8c8d4fbb172daebf8e126
najibjodiansyah <najibjodiansyah11@gmail.com>
2022-02-12 01:26:51 +0700

    slicing component

--- src/App.js
+++ src/App.js
@@ -2,24 +2,29 @@ import React from "react";
 import "./App.css";
 
 function App() {
+	return <div className="Parentbox"></div>;
+}
+
+const FotoProduk = () => {
+	<div className="Foto">
+		<img src="poison-street.jpg" alt="Foto Sepatu"></img>
+	</div>;
+};
+
+function ProdukInfo() {
 	let category = "LOCAL PRIDE SNEAKER SNEAKER";
 	let harga = "8xx.xxx";
 	return (
-		<div class="Parentbox">
-			<div class="Foto">
-				<img src="poison-street.jpg"></img>
-			</div>
-			<div class="Deskripsi">
-				<p class="Cate">{category}</p>
-				<h1 class="Title">Poison Street</h1>
-				<p class="Price">IDR {harga}</p>
-				<p class="Info">
-					Menyita perhatian anak muda. Ini karena ada berbagai macam sneakers
-					yang bertali atau slip on. Menariknya lagi, merk satu ini juga
-					kebanyakan memiliki warna hitam dan putih yang tentu saja bisa dipakai
-					oleh anak-anak sekolah.
-				</p>
-			</div>
+		<div className="Deskripsi">
+			<p className="Cate">{category}</p>
+			<h1 className="Title">Poison Street</h1>
+			<p className="Price">IDR {harga}</p>
+			<p className="Info">
+				Menyita perhatian anak muda. Ini karena ada berbagai macam sneakers yang
+				bertali atau slip on. Menariknya lagi, merk satu ini juga kebanyakan
+				memiliki warna hitam dan putih yang tentu saja bisa dipakai oleh
+				anak-anak sekolah.
+			</p>
 		</div>
 	);
 }
