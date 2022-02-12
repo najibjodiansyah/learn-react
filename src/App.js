3062c3e0a5e4588d5af46a1c7442bdf1b748fa31
najibjodiansyah <najibjodiansyah11@gmail.com>
2022-02-12 16:01:26 +0700

    add argument event

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ function App() {
 	return (
 		<div className="Parentbox">
 			<FotoProduk />
-			<ProdukInfo category="LOCAL PRIDE SNEAKER SNEAKER" harga="8xx.xxx" />
+			<ProdukInfo
+				name="Poison Street"
+				category="LOCAL PRIDE SNEAKER SNEAKER"
+				harga="8xx.xxx"
+			/>
 		</div>
 	);
 }
@@ -23,7 +27,7 @@ function ProdukInfo(props) {
 	return (
 		<div className="Deskripsi">
 			<p className="Cate">{props.category}</p>
-			<h1 className="Title">Poison Street</h1>
+			<h1 className="Title">{props.name}</h1>
 			<p className="Price">IDR {harga}</p>
 			<p className="Info">
 				Menyita perhatian anak muda. Ini karena ada berbagai macam sneakers yang
@@ -31,15 +35,15 @@ function ProdukInfo(props) {
 				memiliki warna hitam dan putih yang tentu saja bisa dipakai oleh
 				anak-anak sekolah.
 			</p>
-			<a onClick={TambahCart} href="#">
+			<a onClick={(e) => TambahCart(props.name, e)} href={props.category}>
 				Add to Cart
 			</a>
 		</div>
 	);
 }
 
-function TambahCart() {
-	return alert("Membeli Product");
+function TambahCart(e) {
+	return alert(`Membeli Product ${e}`);
 }
 
 export default App;
