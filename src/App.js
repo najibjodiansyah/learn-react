adb930b0ecc4fa46f8fbcde135328de91e7fcdcd
najibjodiansyah <najibjodiansyah11@gmail.com>
2022-02-12 16:11:25 +0700

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
 				name="Poison Street"
 				category="LOCAL PRIDE SNEAKER SNEAKER"
 				harga="8xx.xxx"
+				Discount="coming"
 			/>
 		</div>
 	);
@@ -22,13 +23,24 @@ function FotoProduk() {
 	);
 }
 
+function CheckDiscount(props) {
+	const { Discount } = props;
+	if (Discount === "yes") {
+		return <p>DISCOUNT 50 %</p>;
+	} else if (Discount === "coming") {
+		return <p>DISCOUNT IS COMING</p>;
+	}
+	return <p>TIDAK DISCOUNT</p>;
+}
+
 function ProdukInfo(props) {
-	let { harga } = props;
+	let { harga, Discount } = props;
 	return (
 		<div className="Deskripsi">
 			<p className="Cate">{props.category}</p>
 			<h1 className="Title">{props.name}</h1>
 			<p className="Price">IDR {harga}</p>
+			<CheckDiscount Discount={Discount} />
 			<p className="Info">
 				Menyita perhatian anak muda. Ini karena ada berbagai macam sneakers yang
 				bertali atau slip on. Menariknya lagi, merk satu ini juga kebanyakan
